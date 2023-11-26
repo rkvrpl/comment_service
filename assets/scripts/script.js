fed0864edc8807da56bd32d526257bda1ec04860
rkvrpl <rkvrpl@icloud.com>
2023-11-26 19:34:34 +0300

    changed function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@
 
 const userName = document.getElementById('userName');
 const foto = document.getElementById('foto');
-const message = document.getElementById('message');
+const message = document.getElementById('comment');
 const button = document.getElementById('button');
 const resultName = document.getElementById('resultName');
 const resultImg = document.getElementById('resultImg');
@@ -13,28 +13,21 @@ const addTextOnChat = () =>{
     const toLowerCaseName = trimmedName.toLowerCase();
     const correctName = toLowerCaseName.replace(toLowerCaseName[0], toLowerCaseName[0].toUpperCase());
 
-    function checkSpam(message) {
-        let lowerMessage = message.toLowerCase();
-        return lowerMessage.includes('viagra') || lowerMessage.includes('xxx');
-    }
+    // const correctFoto = foto.value;
 
-    resultName.textContent = correctName.value; 
-    resultImg.setAttribute('src', 'foto');
-    resultComment.textContent = checkSpam(message);
+    const toLowerCaseСomment = comment.value.toLowerCase();
+    console.log(toLowerCaseСomment);
+    const correctСomment = toLowerCaseСomment.replace(toLowerCaseСomment[0], toLowerCaseСomment[0].toUpperCase());
+    console.log(correctСomment);
+
+    // function checkSpam(correctСomment) {
+    //     let lowerСomment = correctСomment.toLowerCase();
+    //     return lowerСomment.includes('viagra') || lowerСommente.includes('xxx');
+    // }
+
+    resultName.textContent = correctName; 
+    // resultImg.setAttribute('src', correctFoto);
+    resultComment.textContent = correctСomment;
 };
 
 button.addEventListener('click', addTextOnChat);
-
-
-
-
-// Создадим сервис комментариев. В нём будет три поля ввода:
-// поле для ввода имени. Сделайте интерфейс преобразования имени, который учтёт, все нюансы — лишние пробелы, отсутствие больших букв в имени и прочее. 
-// Например, было введено пользователем : иВаН .
-// Стало: Иван .
-// поле для ввода ссылки на аватар;
-// поле ввода сообщения. Необходимо реализовать отображение и добавление сообщений, а также функцию checkSpam(str), заменяющую 'viagra' или 'XXX' на *** ;
-// функция должна быть нечувствительна к регистру:
-// const comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
-// const comment2 = checkSpam('free xxx'); //результат free ***
-// const comment3 = checkSpam("innocent rabbit") ; //результат innocent rabbit
